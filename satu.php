1997d2d8b325bca31462c4a9e1767eacccdc84e4
astriawati <131867762+astriawati@users.noreply.github.com>
2023-05-12 23:18:57 -0700

    Add files via upload

---
+++ satu.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Selamat Datang ";
+
+$seseorang='Astriawati, s.kom., M.kom.';
+
+echo $seseorang;
+
+echo "<h1> APLIKASI KALKULATOR MANUAL</h1>";
+$n1=10;
+$n2=2;
+$operasi="+";
+if($operasi=="+"){
+    $hasil=$n1+$n2;
+}elseif($operasi=='-'){
+    $hasil=$n1-$n2;
+}elseif($operasi=="*"){
+    $hasil=$n1*$n2;
+}elseif($operasi=="/"){
+    $hasil=$n1/$n2;
+}else{
+    $hasil="kabohe";
+}
+echo "Nilai 1=".$n1.'<br>';
+echo "Nilai 2=".$n2.'<br>';
+echo "operasi".$operasi.'<br>';
+
+echo "Hasilnya ".$hasil;
+
+?>
+
